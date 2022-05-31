78510d4bb29a1f4eff9a3cd9123cfdb754b09a23
githubsupercode <alex.supercode@gmail.com>
2022-05-31 18:48:50 +0200

    lastpush

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// console.log("Hello");
+const example = document.getElementsByClassName("example");
+console.log(example);
+
+function myFunction() {
+    example[0].style.backgroundColor = "black";
+    example[1].style.backgroundColor = "black";
+    example[2].style.backgroundColor = "black";
+    example[3].style.backgroundColor = "black";
+    example[4].style.backgroundColor = "black";
+    example[4].style.color = "white";
+}
